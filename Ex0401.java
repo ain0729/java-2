3b5b39c9390c0e3463a34efc945343d48c6d3954
ain0729 <songaia0729@gmail.com>
2026-05-13 16:18:14 +0900

    yy

---
+++ Ex0401.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Ex0401 {
+   public static void main(String[] args) {
+    
+        Scanner scanner = new Scanner(System.in);
+        //int intArray[;
+        //intArray = new int[5];
+        //선언과 생성을 한줄에 할수있따
+        int intArray[] = new int[5];
+        INT max = 0;
+        System.out.println("5개의 정수를 입력하세요");
+        for (iht i = 0; i < intArray.length; i++) {
+            intArray[i] = scanner.nextInt();
+        }
+        for(int)
+
+}
